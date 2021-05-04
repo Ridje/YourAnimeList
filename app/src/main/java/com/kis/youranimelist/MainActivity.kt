b802b04f161b5e7da90a4e915c451b5d9f9f2b4b
ridje <angelnumber59@gmail.com>
2021-05-04 17:27:35 +0300

    Added button

--- app/src/main/java/com/kis/youranimelist/MainActivity.kt
+++ app/src/main/java/com/kis/youranimelist/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kis.youranimelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var generalViewGroup:ConstraintLayout;
+    lateinit var button:MaterialButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        generalViewGroup = findViewById(R.id.activity_main_layout);
+        button = MaterialButton(this);
+        button.text = "Start button";
+        button.setOnClickListener({ button.text = "Processing..."})
+        generalViewGroup.addView(button);
     }
 }
