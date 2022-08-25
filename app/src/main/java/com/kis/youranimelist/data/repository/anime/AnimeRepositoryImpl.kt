f2e4c926bf9fbdf2b65d44640ed774ad2b9b3f51
ridje <ridje@MacBook-Pro-ridje.local>
2022-08-25 20:03:50 +0400

    Fixed anime cache in loading data.

--- app/src/main/java/com/kis/youranimelist/data/repository/anime/AnimeRepositoryImpl.kt
+++ app/src/main/java/com/kis/youranimelist/data/repository/anime/AnimeRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.kis.youranimelist.data.repository.anime
 
+import com.kis.youranimelist.data.cache.AnimeRankingMemoryCache
 import com.kis.youranimelist.data.cache.model.PicturePersistence
 import com.kis.youranimelist.data.network.model.TokenResponse
 import com.kis.youranimelist.data.network.model.ranking_response.RankingRootResponse
@@ -17,6 +18,7 @@ class AnimeRepositoryImpl(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource,
     private val animeMapper: AnimeMapper,
+    private val cache: AnimeRankingMemoryCache.Factory,
 ) : AnimeRepository {
 
     override suspend fun getRankingAnimeList(
@@ -25,9 +27,19 @@ class AnimeRepositoryImpl(
         offset: Int?,
     ): ResultWrapper<List<Anime>> {
 
+        cache.getOrCreate(rankingType).cache?.get(offset ?: 0)?.let { cachedResult ->
+            return ResultWrapper.Success(cachedResult)
+        }
+
         return remoteDataSource
             .getAnimeRankingList(rankingType, limit, offset)
-            .asResult { from: RankingRootResponse -> from.data.map { Anime(it.anime) } }
+            .asResult { from: RankingRootResponse -> from.data.map { animeMapper.map(it) } }
+            .also { remoteResult ->
+                if (remoteResult is ResultWrapper.Success) {
+                    cache.getOrCreate(rankingType).updateCache(offset ?: 0, remoteResult.data)
+                    remoteResult.data.forEach { localDataSource.saveAnimeToCache(it) }
+                }
+            }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
--- app/src/main/java/com/kis/youranimelist/data/repository/animeranking/AnimeRankingRepositoryImpl.kt
+++ app/src/main/java/com/kis/youranimelist/data/repository/animeranking/AnimeRankingRepositoryImpl.kt
@@ -4,24 +4,24 @@ import android.accounts.NetworkErrorException
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.haroldadmin.cnradapter.NetworkResponse
+import com.kis.youranimelist.data.cache.AnimeRankingMemoryCache
 import com.kis.youranimelist.data.network.model.ErrorResponse
 import com.kis.youranimelist.data.network.model.ranking_response.RankingRootResponse
 import com.kis.youranimelist.data.repository.LocalDataSource
 import com.kis.youranimelist.data.repository.RemoteDataSource
 import com.kis.youranimelist.domain.rankinglist.mapper.AnimeMapper
 import com.kis.youranimelist.domain.rankinglist.model.Anime
-import java.lang.RuntimeException
 import javax.inject.Inject
 
 class AnimeRankingRepositoryImpl @Inject constructor(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource,
     private val animeMapper: AnimeMapper,
+    private val cacheFactory: AnimeRankingMemoryCache.Factory,
 ) : AnimeRankingRepository {
 
-    private val listCache = HashMap<Int, List<Anime>>()
-
     override fun getDataSource(rankingType: String): PagingSource<Int, Anime> {
+        cacheFactory.getOrCreate(rankingType).invalidateCache()
         return object : PagingSource<Int, Anime>() {
             override fun getRefreshKey(state: PagingState<Int, Anime>): Int? {
                 return state.anchorPosition
@@ -29,8 +29,8 @@ class AnimeRankingRepositoryImpl @Inject constructor(
 
             override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Anime> {
                 val nextPageKey = params.key ?: 0
-                val cachedList = listCache[nextPageKey]
-                return if ( cachedList != null) {
+                val cachedList = cacheFactory.getOrCreate(rankingType).cache?.get(nextPageKey)
+                return if (cachedList != null) {
                     LoadResult.Page(
                         data = cachedList,
                         prevKey = if (nextPageKey == 0) null else nextPageKey - cachedList.size,
@@ -43,7 +43,8 @@ class AnimeRankingRepositoryImpl @Inject constructor(
                                 .map { animeMapper.map(it) }
                                 .onEach { localDataSource.saveAnimeToCache(it) }
                                 .run {
-                                    listCache[nextPageKey] = this
+                                    cacheFactory.getOrCreate(rankingType)
+                                        .updateCache(nextPageKey, this)
                                     LoadResult.Page(
                                         data = this,
                                         prevKey = if (nextPageKey == 0) null else nextPageKey - this.size,
