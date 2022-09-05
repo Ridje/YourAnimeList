9f6e7c576f8dd625344cfd6311d3caf50fc5303a
ridje <angelnumber59@gmail.com>
2022-09-05 23:03:25 +0400

    Added foreground service info for backward compatability.

--- app/src/main/java/com/kis/youranimelist/data/SyncWorker.kt
+++ app/src/main/java/com/kis/youranimelist/data/SyncWorker.kt
@@ -1,18 +1,27 @@
 package com.kis.youranimelist.data
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkerParameters
+import com.kis.youranimelist.R
 import com.kis.youranimelist.data.repository.personalanime.PersonalAnimeRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
+private const val SYNC_NOTIFICATION_ID = 0
+private const val SYNC_NOTIFICATION_CHANNEL_ID = "SyncNotificationChannel"
 
 @HiltWorker
 class SyncWorker @AssistedInject constructor(
@@ -21,6 +30,10 @@ class SyncWorker @AssistedInject constructor(
     private val personalAnimeRepository: PersonalAnimeRepository,
     private val dispatchers: Dispatchers,
 ) : CoroutineWorker(applicationContext, workerParams) {
+
+    override suspend fun getForegroundInfo(): ForegroundInfo =
+        applicationContext.syncForegroundInfo()
+
     override suspend fun doWork(): Result {
         val result = withContext(dispatchers.IO) {
             personalAnimeRepository.synchronize()
@@ -33,10 +46,38 @@ class SyncWorker @AssistedInject constructor(
             Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
         const val SyncWorkName = "SyncWorkName"
 
+
         fun startSyncJob() =
             OneTimeWorkRequestBuilder<SyncWorker>()
                 .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                 .setConstraints(constraints)
                 .build()
     }
+}
+
+fun Context.syncForegroundInfo() = ForegroundInfo(
+    SYNC_NOTIFICATION_ID,
+    syncWorkNotification()
+)
+
+private fun Context.syncWorkNotification(): Notification {
+    val channel = NotificationChannel(
+        SYNC_NOTIFICATION_CHANNEL_ID,
+        getString(R.string.sync_notification_channel_name),
+        NotificationManager.IMPORTANCE_DEFAULT
+    ).apply {
+        description = getString(R.string.sync_notification_channel_description)
+    }
+    // Register the channel with the system
+    val notificationManager: NotificationManager? =
+        getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
+    notificationManager?.createNotificationChannel(channel)
+
+    return NotificationCompat.Builder(this, SYNC_NOTIFICATION_CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_myanimelist)
+        .setContentTitle(getString(R.string.sync_notification_title))
+        .setProgress(0, 0, true)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .build()
 }
